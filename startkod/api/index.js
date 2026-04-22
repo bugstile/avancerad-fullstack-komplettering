3f572688bab298181a550610d874d88ae588fbfb
bugstile <bugstile@hotmail.com>
2026-04-22 23:45:42 +0200

    test: lägg till CORS stöd för API ✔

--- startkod/api/index.js
+++ startkod/api/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 const HOST = '0.0.0.0';
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
